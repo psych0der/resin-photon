b27949f9dbabf993937399b9d7d2118255d37462
psych0der <mayankbhola@gmail.com>
2018-07-26 02:26:16 +0530

    add dashboard to home

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,14 +1,56 @@
+// @flow
 import React from 'react';
-import PropTypes from 'prop-types';
-
+import { RadialSlider, IosSwitch } from '../../components';
+import { Dashboard } from '../../containers';
+import { getFormatedDate, formatAMPM } from '../../commons/helpers';
+import { Navbar, DropDownButton, Img, Link } from 'rendition';
 import styles from './index.css';
+type Props = {};
+type State = {
+  changeValue: number,
+};
 
-export class Home extends React.Component {
+export class Home extends React.Component<Props, State> {
+  state = { changeValue: 0 };
+  handleChange = changeValue => {
+    this.setState({ changeValue });
+  };
   render() {
-    return <div className={styles.someClass}>Home</div>;
+    const now = new Date();
+    const navBackgrounColor = '#222222';
+    const navBarTextColor = '#aaa';
+    const currentDate = <div>{getFormatedDate(now)}</div>;
+    const currentTime = (
+      <span
+        style={{
+          position: 'absolute',
+          display: 'inlineBlock',
+          textAlign: 'center',
+          left: '50%',
+        }}
+      >
+        {formatAMPM(now)}
+      </span>
+    );
+    return (
+      <div className={styles.someClass}>
+        <Navbar
+          color="white"
+          brand={currentDate}
+          bg={navBackgrounColor}
+          p="10px"
+          style={{ height: '50px' }}
+          fontSize="12px"
+          color={navBarTextColor}
+        >
+          {currentTime}
+          <Link />
+          <Link />
+        </Navbar>
+        <Dashboard />
+      </div>
+    );
   }
 }
-
-Home.propTypes = {};
 
 export default Home;
