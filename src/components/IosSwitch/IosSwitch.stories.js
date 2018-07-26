70ef4165676bb8533f364a28378bc86bc3d994ce
psych0der <mayankbhola@gmail.com>
2018-07-26 05:37:43 +0530

    Fix checked case

--- src/components/IosSwitch/IosSwitch.stories.js
+++ src/components/IosSwitch/IosSwitch.stories.js
@@ -7,7 +7,7 @@ import IosSwitch from './index';
 
 storiesOf('IosSwitch', module)
   .add('visible state', () => <IosSwitch />)
-  .add('Checked ', () => <IosSwitch defaultChecked={true} />)
+  .add('Checked ', () => <IosSwitch checked={true} />)
   .add('small size', () => <IosSwitch small={true} />)
   .add('With custom handle change function', () => (
     <IosSwitch handleChange={action('checked')} />
